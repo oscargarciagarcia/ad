6448626c419137a8f8461cd8ad70fbe0af05a258
oscargarciagarcia <2dam.ogarcia@gmail.com>
2015-12-10 10:27:55 +0100

    Añadido GHolaMundo

---
+++ GHolaMundo/src/main/java/org/institutoserpis/ad/HolaMundo.java
@@ -0,0 +1,9 @@
+package org.institutoserpis.ad;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo desde eclipse con gradle, Oscar!");
+	}
+
+}
