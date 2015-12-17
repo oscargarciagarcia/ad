376f6c05605c6bbe0ce3cc06393487b239a96887
oscargarciagarcia <2dam.ogarcia@gmail.com>
2015-12-17 10:21:41 +0100

    PruebaArticulo(GMySql): Insert añadido y funciona

--- GMySql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
+++ GMySql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
@@ -1,21 +1,50 @@
 package org.institutoserpis.ad;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class PruebaArticulo {
 	public static void main(String[] args) throws SQLException {
+		
+		
 		//conectar con la base de datos 
-		Connection connection = DriverManager.getConnection(
+		Connection conexion = DriverManager.getConnection(
 		"jdbc:mysql://localhost/dbprueba", "root", "sistemas");
 			
-		Statement statement = connection.createStatement();
-		//ejecutamos la sentencia select * from articulo
-		ResultSet result = statement.executeQuery("SELECT * FROM articulo");
+		Statement stat = conexion.createStatement();
+		
+		//Insert
+		String sqlInsert = " INSERT INTO articulo VALUES (null,?,?,?)";
+		PreparedStatement insertar = null;
+		insertar = (PreparedStatement)conexion.prepareStatement(sqlInsert);
+		//Damos valor a los atributos de la tabla
+		Scanner tcl = new Scanner(System.in);
+		//nombre
+		System.out.print("Introduce el nombre: ");
+		String nombreInsert = tcl.nextLine();
+		insertar.setString(1, nombreInsert);
+		//categoria
+		System.out.print("Introduce la categoria (1, 2 o 3): ");
+		int categoriaInsert = tcl.nextInt();
+		insertar.setInt(2, categoriaInsert);
+		//precio
+		System.out.print("Introduce el precio: ");
+		Double precioInsert = tcl.nextDouble();
+		insertar.setDouble(3, precioInsert);
+		insertar.executeUpdate();
+		
+		//String sqlUpdate = " UPDATE articulo SET "
+		//stat.executeUpdate(sql);
+		//Muestro lo que quiero ver de la tabla
+		ResultSet result = stat.executeQuery("SELECT * FROM articulo");
+		
+	
 			
 		ResultSetMetaData rsMetaData = result.getMetaData();
 		int numColum = rsMetaData.getColumnCount();
@@ -29,16 +58,19 @@ public class PruebaArticulo {
 	
 		System.out.println();
 		while(result.next()){
-			String id1 = result.getString(1);
-			String nombre1 = result.getString(2);
-			String categoria1= result.getString(3);
-			String precio1 = result.getString(4);
+			String id = result.getString(1);
+			String nombre = result.getString(2);
+			String categoria= result.getString(3);
+			String precio = result.getString(4);
+			
 			//mostramos los datos de las columnas de la base de datos
-			System.out.println(id1+" | "+nombre1+" | "+ categoria1 +" | "+ precio1);
+			System.out.println(id+" | "+nombre+" | "+ categoria +" | "+ precio);
+			
+			
 		}
 			
 			
-		connection.close();
+		conexion.close();
 		System.out.println();
 		System.out.println("Fin");
 		}
