dd081532e5fdda3476f6a0130b6ad8a09dc63208
oscargarciagarcia <2dam.ogarcia@gmail.com>
2016-01-07 10:41:31 +0100

    Prueba articulo modificado: ordenado, metodos creados, menu funcionando bien.

--- GMySql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
+++ GMySql/src/main/java/org/institutoserpis/ad/PruebaArticulo.java
@@ -10,24 +10,85 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class PruebaArticulo {
-	public static void main(String[] args) throws SQLException {
+	//Scanner creado
+	static Scanner tcl = new Scanner(System.in);
+	
+	public static void main(String[] args) throws Exception {
 		
-		
-		//conectar con la base de datos 
+		//conectar con la base de datos
 		Connection conexion = DriverManager.getConnection(
-		"jdbc:mysql://localhost/dbprueba", "root", "sistemas");
-			
+			"jdbc:mysql://localhost/dbprueba", "root", "sistemas");
 		Statement stat = conexion.createStatement();
 		
-		Scanner tcl = new Scanner(System.in);
+		//Muestro la tabla
+		//ResultSet result1 = stat.executeQuery("SELECT * FROM artiuclo");
+		//mostrarDatos(result1);
 		
-		//System.out.println("Que quieres hacer? ");
-		//int opcion = tcl.nextInt();
-		//if (opcion == 1){
-		//Insert
-		String sqlInsert = " INSERT INTO articulo VALUES (null,?,?,?)";
-		PreparedStatement insertar = null;
-		insertar = (PreparedStatement)conexion.prepareStatement(sqlInsert);
+		int opcion;
+		//Menu
+	do {	
+		System.out.println("1. Insertar");
+		System.out.println("2. Borrar");
+		System.out.println("3. Mostrar Tabla");
+		System.out.println("4. Salir");
+		System.out.println("");
+		System.out.println("Que quieres hacer? ");
+		opcion = tcl.nextInt();
+		//con el nextLine() limpio el buffer
+		tcl.nextLine();
+		
+		//Opcion 1: Insertar datos
+		if (opcion == 1){
+			String sqlInsert = "INSERT INTO articulo VALUES (null,?,?,?)";
+			PreparedStatement insertar = (PreparedStatement)conexion.prepareStatement(sqlInsert);
+			insertarDatos(insertar);
+			
+		//Opcion 2: Borrar datos		
+		}if (opcion == 2){
+			String sqlDelete = "DELETE FROM articulo WHERE id=?";
+			PreparedStatement borrar = (PreparedStatement)conexion.prepareStatement(sqlDelete);
+			borrarDatos(borrar);
+		
+		//Opción 3: Mostar los datos
+		}if (opcion == 3){
+			ResultSet result = stat.executeQuery("SELECT * FROM articulo");
+			mostrarDatos(result);
+		}else{	
+			
+		
+		}
+	}while (opcion != 4);
+	conexion.close();
+	System.out.println();
+	System.out.println("Fin");
+}
+	//Metodo Mostrar
+	private static void mostrarDatos(ResultSet resultSet) throws SQLException {
+		//esto muestra el nombre de las columnas	
+				ResultSetMetaData rsMetaData = resultSet.getMetaData();
+				int numColum = rsMetaData.getColumnCount();
+				System.out.println("Nombre de las columnas:");
+				for (int i =1; i < numColum+1; i++) {
+					String columnName = rsMetaData.getColumnName(i);
+					System.out.print(columnName+"  ");
+					    
+				}
+				System.out.println();
+			
+				System.out.println();
+				while(resultSet.next()){
+					//le indico que coja de la sentencia "result" las columnas
+					String id = resultSet.getString(1);
+					String nombre = resultSet.getString(2);
+					String categoria= resultSet.getString(3);
+					String precio = resultSet.getString(4);
+				
+					//mostramos los datos de las columnas de la base de datos
+					System.out.println(id+" | "+nombre+" | "+ categoria +" | "+ precio);
+				}
+	}
+	//Metodo Insert		
+	private static int insertarDatos(PreparedStatement insertar) throws SQLException {
 		//nombre
 		System.out.print("Introduce el nombre: ");
 		String nombreInsert = tcl.nextLine();
@@ -40,52 +101,19 @@ public class PruebaArticulo {
 		System.out.print("Introduce el precio: ");
 		Double precioInsert = tcl.nextDouble();
 		insertar.setDouble(3, precioInsert);
-		insertar.executeUpdate();
-		//}else{
-		//String sqlUpdate = " UPDATE articulo SET "
-		//stat.executeUpdate(sql);
-		
-		//Delete
-		String sqlDelete = "DELETE FROM articulo WHERE id=?";
-		PreparedStatement borrar = null;
-		borrar = (PreparedStatement)conexion.prepareStatement(sqlDelete);
+		return insertar.executeUpdate();	
+	}
+	//Metodo Borrar
+	private static int borrarDatos(PreparedStatement borrar) throws SQLException {
 		System.out.println("Que id quieres borrar? ");
 		int idBorrar = tcl.nextInt();
 		borrar.setInt(1, idBorrar);
-		borrar.executeUpdate();
-		//}
-		//Muestro lo que quiero ver de la tabla
-		ResultSet result = stat.executeQuery("SELECT * FROM articulo");
+		return borrar.executeUpdate();
+	}
+	//Metodo Upadte
+	//private static int updateDatos (PreparedStatement updatear) throws SQLException {
+	//	System.out.println("Que articulo quieres modificar");
 		
+	//}
 	
-		//esto muestra el nombre de las columnas	
-		ResultSetMetaData rsMetaData = result.getMetaData();
-		int numColum = rsMetaData.getColumnCount();
-		System.out.println("Nombre de las columnas:");
-		for (int i =1; i < numColum+1; i++) {
-			String columnName = rsMetaData.getColumnName(i);
-			System.out.print(columnName+"  ");
-			    
-		}
-		System.out.println();
-	
-		System.out.println();
-		while(result.next()){
-			//le indico que coja de la sentencia "result" las columnas
-			String id = result.getString(1);
-			String nombre = result.getString(2);
-			String categoria= result.getString(3);
-			String precio = result.getString(4);
-			
-			//mostramos los datos de las columnas de la base de datos
-			System.out.println(id+" | "+nombre+" | "+ categoria +" | "+ precio);
-			
-			
-		}
-			
-			
-		conexion.close();
-		System.out.println();
-		System.out.println("Fin");
-		}
 }
